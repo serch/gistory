37a6f8d1f2690d447455bb3a5a12fdd844a1b394
Sergio Medina <sergio.medina@offerista.com>
2017-02-07 19:02:02 +0100

    show also when current version was introduced

--- lib/gistory/history.rb
+++ lib/gistory/history.rb
@@ -16,11 +16,14 @@ module Gistory
     def history(gem_name)
       raise(Gistory::Error, 'No gem name provided') if gem_name.nil?
 
+      puts "Gem: #{gem_name}"
       lockfile = Bundler::LockfileParser.new(Bundler.read_file('Gemfile.lock'))
       the_gem  = lockfile.specs.find { |s| s.name == gem_name }
-      puts "current #{the_gem.version}"
-      current = the_gem.version.to_s
+      puts "Current version: #{the_gem.version}"
+      puts ''
 
+      puts 'Change history:'
+      previous_version = nil
       commits = `git log --pretty=format:"%H|%cD" --max-count=100 --follow Gemfile.lock`
       commits.split("\n").each do |c|
         commit, date = c.split('|')
@@ -28,12 +31,20 @@ module Gistory
         lockfile = Bundler::LockfileParser.new(file_content)
         the_gem = lockfile.specs.find { |s| s.name == gem_name }
 
+        # we got to the end, the gem didnt exist back then
+        # what if it was added then removed and then added again???
         if the_gem.nil?
           puts 'the end'
           break
-        elsif the_gem.version.to_s != current # only print it if it changed
-          current = the_gem.version.to_s
-          puts "#{the_gem.version} on #{date} (commit #{commit})"
+        end
+
+        if previous_version.nil?
+          # first commit
+          previous_version = the_gem.version.to_s
+          puts "#{the_gem.version} on #{date} (commit #{commit[0..6]})"
+        elsif the_gem.version.to_s != previous_version # only print it if it changed
+          previous_version = the_gem.version.to_s
+          puts "#{the_gem.version} on #{date} (commit #{commit[0..6]})"
         end
       end
     end
