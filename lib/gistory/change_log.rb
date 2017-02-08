a7ae46c5d442bd1fd46ec9e7ab5da1118865d50d
Sergio Medina <medina@checkitmobile.com>
2017-02-08 21:15:03 +0100

    make smaller methods

--- lib/gistory/change_log.rb
+++ lib/gistory/change_log.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_literal: truew
 require 'bundler'
 
 module Gistory
@@ -12,19 +12,16 @@ module Gistory
     def changelog_for_gem(gem_name)
       version_changes = []
       previous_version = nil
-
       lockfile_changes = @repo.changes_to_file(LOCKFILE)
 
       lockfile_changes.each do |commit|
-        lockfile_content = @repo.file_content_at_commit(commit.short_hash, LOCKFILE)
-        lockfile = Bundler::LockfileParser.new(lockfile_content)
-        gem_spec = lockfile.specs.find { |spec| spec.name == gem_name }
+        gem_spec = gem_spec_at_commit_hash(commit.short_hash)
 
-        # we got to the end, the gem didnt exist back then
-        # what if it was added then removed and then added again???
+        # we reached the end, the gem didn't exist back then
+        # TODO: what if it was added then removed and then added again?
         break if gem_spec.nil?
 
-        # only save it if the version changed
+        # only store version changes of this gem
         unless gem_spec.version.to_s == previous_version
           version_changes << VersionChange.new(commit: commit, version: gem_spec.version)
           previous_version = gem_spec.version.to_s
@@ -32,6 +29,15 @@ module Gistory
       end
 
       version_changes
+    end
+
+    private
+
+    def gem_spec_at_commit_hash(commit_hash)
+      lockfile_content = @repo.file_content_at_commit(commit_hash, LOCKFILE)
+      lockfile = Bundler::LockfileParser.new(lockfile_content)
+      gem_spec = lockfile.specs.find { |spec| spec.name == gem_name }
+      gem_spec
     end
   end
 end
