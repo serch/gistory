28dc4a6caed0cafabffbe5df503ab6d68353e62e
Sergio Medina <sergio.medina@offerista.com>
2017-02-08 18:19:24 +0100

    parsing arguments wip

--- lib/gistory/history.rb
+++ lib/gistory/history.rb
@@ -1,3 +1,6 @@
+require 'optparse'
+require 'ostruct'
+
 module Gistory
   class History
     def initialize(repo:, args:)
@@ -6,9 +9,45 @@ module Gistory
     end
 
     def run
-      history(@args.shift)
+      options = parse(@args)
+      history(options.gem_name)
     rescue Gistory::Error => error
       puts error.message
+    end
+
+    def parse(args)
+      options = OpenStruct.new
+      options.max_lockfile_changes = 100
+
+      opt_parser = OptionParser.new do |opts|
+        opts.banner = 'Usage: gistory <gem_name> [options]'
+
+        opts.separator ''
+        opts.separator 'Specific options:'
+
+        opts.on('-m', '--max-lockfile-changes [INTEGER]', Integer, 'max number of changes to the lock file (default 100)') do |m|
+          options.max_lockfile_changes = m
+        end
+
+        opts.separator ''
+        opts.separator 'Common options:'
+
+        opts.on_tail('-h', '--help', 'Show this message') do
+          puts opts
+          exit
+        end
+
+        opts.on_tail('--version', 'Show version') do
+          puts Gistory::VERSION
+          exit
+        end
+      end
+
+      opt_parser.parse!(args)
+      gem_name = args.pop
+      raise 'no gem name specified' unless gem_name
+      options.gem_name = gem_name
+      options
     end
 
     def history(gem_name)
