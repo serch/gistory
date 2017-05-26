981937653ab747bc56678aa0148c37b4c2d00cc3
Sergio Medina <sergio.medina@textmaster.com>
2017-05-26 10:50:16 +0200

    replace deprecated::[] with ::new([])

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,10 +2,10 @@
 require 'simplecov'
 require 'coveralls'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
-]
+])
 SimpleCov.start
 
 $LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
