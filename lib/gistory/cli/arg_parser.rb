a433243a58cc3700a4c29980dea16d186aefe506
Sergio Medina <sme@monsido.com>
2020-06-27 22:25:43 +0200

    fix help info

--- lib/gistory/cli/arg_parser.rb
+++ lib/gistory/cli/arg_parser.rb
@@ -65,7 +65,7 @@ module Gistory
       def add_use_commits_from_all_branches(parser, config)
         description = 'use commits from all branches ' \
                       '(by default it uses only commits made to the current branch)'
-        parser.on('-a', '--all-commits', description) do |a|
+        parser.on('-a', '--all-branches', description) do |a|
           config.all_branches = a
         end
       end
